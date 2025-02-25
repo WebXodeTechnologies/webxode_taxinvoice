be711ad75c4ce7f009795a1de933b510776a89ba
ak220193 <mail2meak22frcrio@gmail.com>
2025-02-25 07:41:40 +0530

    Onboarding Route

---
+++ tax-invoice/app/utils/zodSchemas.ts
@@ -0,0 +1,6 @@
+import {z} from "zod"
+export const onboardingSchema = z.object({
+    firstName: z.string().min(2, "First Name is Required"),
+    lastName: z.string().min(2,"Last Name is Required"),
+    address: z.string().min(2, "Address is Required"),
+})
